fe2b1e30d674c114e6bf253d7d1172ad3b3a5153
Pedro Costa <96047553+pcesab@users.noreply.github.com>
2025-02-14 19:48:22 +0000

    Correção de erros

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Queue1
                 "Coimbra", "Lisboa", "Porto", "Faro"
             */
 
-            string cidades = { "Coimbra", "Lisboa", "Porto", "Faro" };
+            string[] cidades = { "Coimbra", "Lisboa", "Porto", "Faro" };
 
             /*
                 2 - Completar a declaração de uma queue, chamada cidades2, do tipo string 
